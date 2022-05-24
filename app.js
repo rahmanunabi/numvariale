ae8f26247588ec1d8585b9fa73ef1b802c6e9055
rahmanunabi <102538438+rahmanunabi@users.noreply.github.com>
2022-05-24 16:46:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var age = prompt ("enter your age");
+alert("I am" + " " + age + " " + "years old");
+alert("You have visited this site 21 times.");
+var birthYear = prompt ("your birth year");
+document.write("My birth year is" + " " + birthYear + "<br/>");
+document.write("Data type of my declared variable is  number" + "<br/>");
+var visitorName = prompt("Visitor's Name");
+document.write(visitorName + " ");
+var productTitle = prompt("Product Title");
+var quantity = prompt("how many products you want to order")
+document.write("ordered" + " " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
